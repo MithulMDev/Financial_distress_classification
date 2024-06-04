04a71c933ce2b66fc53d887fc5b3f903c9739337
unknown <mithulapc6@gmail.com>
2024-06-04 14:21:54 +0530

    Initial commit

---
+++ runner.py
@@ -0,0 +1,56 @@
+# import libraries
+import pandas as pd
+import numpy as np
+from datetime import datatime
+
+
+# Read the data
+alldf = pd.read_excel()
+
+
+df = # remove all the unnecessary columns
+
+df1 = # remove a threshold of null values and let the rest be saved
+df2 = ""
+df3 = ""
+
+
+dflist = [df1, df2, df3]
+
+def imputer(): # use for implementing imputation techniques
+    # get the input argument and try running it here
+    # have separate segments for supporting each of the imputer if necessary
+
+
+ratio = # [ columns ]  # grouping the variables under the overheads
+
+
+def dimensionalityreduction(): # for implementing PCA
+
+
+def modeltrainer(): # for implementing and training algorithms
+    for e in models:
+        # train and save the model
+
+
+def evaluation():
+    # give all the evaluation and return the results
+
+
+def main():
+
+    imputerlist = [
+        # give all the list of imputers that are going to be used
+    ]
+
+    dimensionRlist = [
+        # include the PCA object
+    ]
+
+
+    for e in imputerlist:
+        for i in dimensionRlist:
+
+            # implement by calling the functionalities
+
+
