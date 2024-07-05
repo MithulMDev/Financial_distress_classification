6998246f46af38bbc5e574a50df8e001d7bc9dde
unknown <mithulapc6@gmail.com>
2024-07-05 22:36:40 +0530

    Added notebooks

--- code_teleport/simple_thing.py
+++ code_teleport/simple_thing.py
@@ -4,4 +4,9 @@ A simple thing of switching tabs and mouse clicks
 let the logs run in random intervals and then open necessary apps
 
 let the mouse click at random intervals and then switch tabs at random intervals without both interfering each other. 
+
+
+Tip:
+random pace of mouse movements, random speed key presses of random words
 """
+
